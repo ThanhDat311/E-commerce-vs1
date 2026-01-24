a700b8a011086c3dbf4ceb920c3d12aef3c9c35a
ThanhDat <thanhdat2005@gmail.com>
2026-01-24 16:27:47 +0700

    a

--- app/Services/Payment/VnpayGateway.php
+++ app/Services/Payment/VnpayGateway.php
@@ -28,7 +28,7 @@ class VnpayGateway implements PaymentGatewayInterface
         $inputData = array(
             "vnp_Version" => "2.1.0",
             "vnp_TmnCode" => $vnp_TmnCode,
-            "vnp_Amount" => $vnp_Amount * 100,
+            "vnp_Amount" => $vnp_Amount * 1000,
             "vnp_Command" => "pay",
             "vnp_CreateDate" => date('YmdHis'),
             "vnp_CurrCode" => "VND",
